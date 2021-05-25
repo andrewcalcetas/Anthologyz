b1f81d9b4bc7fd55bdd45f66fdd8889a05c8732d
mehroz <mehroz.akhtar@student.rjuhsd.us>
2021-05-25 16:01:17 -0700

    productfinder now works only with select sites

--- product-finder.js
+++ product-finder.js
@@ -1,11 +1,16 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    var productTitle = tabs[0].title;   //title
+    if (tabs[0].url.search('/dp/') !== -1 || tabs[0].url.search('/ip/') !== -1 || tabs[0].url.search('/p/') !== -1){
+        var productTitle = tabs[0].title;   //title
 
-    productTitle=productTitle.replace('Amazon.com', '')
-    productTitle=productTitle.replace('Target', '')
-    productTitle=productTitle.replace('Walmart.com - ', '')
-    productTitle=productTitle.replace('Walmart.com', '')
-    
-    document.getElementById('productTitle').innerText = productTitle 
-    console.log(productTitle)
+        productTitle = productTitle.replace('Amazon.com', '')
+        productTitle = productTitle.replace('Target', '')
+        productTitle = productTitle.replace('Walmart.com - ', '')
+        productTitle = productTitle.replace('Walmart.com', '')
+
+        document.getElementById('productTitle').innerText = productTitle 
+        console.log(productTitle)
+    }
+    else{
+
+    }
 });
