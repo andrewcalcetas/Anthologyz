0a746d2520abb9c477a883fb8bda6b087cb7de2a
mehroz <mehroz.akhtar@student.rjuhsd.us>
2021-05-25 15:54:46 -0700

    removed websites from productTitle

--- product-finder.js
+++ product-finder.js
@@ -1,5 +1,11 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     var productTitle = tabs[0].title;   //title
+
+    productTitle=productTitle.replace('Amazon.com', '')
+    productTitle=productTitle.replace('Target', '')
+    productTitle=productTitle.replace('Walmart.com - ', '')
+    productTitle=productTitle.replace('Walmart.com', '')
+    
     document.getElementById('productTitle').innerText = productTitle 
     console.log(productTitle)
 });
